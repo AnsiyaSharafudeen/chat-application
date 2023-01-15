20280569fb2929938e9b21591fb054dc4651273d
AnsiyaSharafudeen <ansiyas1997@gmail.com>
2023-01-15 19:12:21 +0400

    home features done

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+
+const connectDB = async () => {
+    mongoose.set("strictQuery", false);
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser : true,
+        })
+        console.log(`MongoDB connected : ${conn.connection.host} `)
+    } catch (error) {
+        console.log(`Error : ${error.message}`)
+        process.exit()
+        
+    }
+}
+
+module.exports = connectDB;
